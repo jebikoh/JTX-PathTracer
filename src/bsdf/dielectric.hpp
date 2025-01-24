3b21df48c351e382a0fff2c374284f7c94fb6860
Jayden E <aedara@uw.edu>
2025-01-24 14:51:18 -0800

    Add thin dielectric BxDF

--- src/bsdf/dielectric.hpp
+++ src/bsdf/dielectric.hpp
@@ -46,3 +46,47 @@ public:
 private:
     float eta;
 };
+
+class ThinDielectricBxDF {
+public:
+    explicit ThinDielectricBxDF(const float eta) : eta(eta) {}
+
+    [[nodiscard]] Vec3 evaluate(const Vec3 &w_o, const Vec3 &w_i) const {
+        return {};
+    }
+
+    BSDFSample sample(const Vec3 &w_o, float uc, const Vec2f &u) const {
+        // Only handling perfectly specular case for now
+
+        // Calculate Fresnel reflectance and complementary transmission
+        float R = fresnelDielectric(jtx::cosTheta(w_o), eta);
+        float T = 1 - R;
+
+        if (R < 1) {
+            R += (T * T * R) / (1 - R * R);
+            T = 1 - R;
+        }
+
+        // Pick between reflectance and transmission
+        const float p = R / (R + T);
+        if (uc < p) {
+            // Reflectance: sample BRDF
+            const Vec3 w_i = {-w_o.x, -w_o.y, w_o.z};
+            const Vec3 f{R / jtx::absCosTheta(w_i)};
+
+            return {f, w_i, p};
+        } else {
+            // Transmission: sample BTDF
+            Vec3 w_i = -w_o;
+            Vec3 f{T / jtx::absCosTheta(w_i)};
+            return {f, w_i, 1 - p};
+        }
+    }
+
+    [[nodiscard]] float pdf(const Vec3 &w_o, const Vec3 &w_i) const {
+        return 0;
+    }
+private:
+    float eta;
+
+};

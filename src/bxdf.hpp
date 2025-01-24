ef021218f81065133057dbbff439261159f92ff1
Jayden E. <aneeshedara@gmail.com>
2025-01-23 20:30:27 -0800

    neesh@Edelgard MINGW64 ~ $ cd Documents/Projects/raytracing/
    Aneesh@Edelgard MINGW64 ~/Documents/Projects/raytracing (main)
    $ cd ..
    
    Aneesh@Edelgard MINGW64 ~/Documents/Projects
    $ rm -rf raytracing/
    
    Aneesh@Edelgard MINGW64 ~/Documents/Projects
    $ git clone ^[[200~https://github.com/jebikoh/raytracing.git~^C
    
    Aneesh@Edelgard MINGW64 ~/Documents/Projects
    $ git clone
    Add fresnel for dielectrics

--- src/bxdf.hpp
+++ src/bxdf.hpp
@@ -5,26 +5,68 @@
 #include "sampling.hpp"
 #include <jtxlib/util/taggedptr.hpp>
 
-inline Vec3 reflect(const Vec3& w_o, const Vec3& n) {
+inline Vec3 reflect(const Vec3 &w_o, const Vec3 &n) {
     return -w_o + 2 * jtx::dot(w_o, n) * n;
 }
 
+/**
+ * Computes refraction direction w_i via Snell's Law.
+ * @param w_i incident direction
+ * @param n surface normal
+ * @param eta relative index of refraction: eta_t / eta_i
+ * @param eta_p relative eta used after checking ray direction
+ * @param w_t refraction direction
+ * @return true in case refraction, false in case total internal reflection
+ */
 inline bool refract(const Vec3 &w_i, Vec3 n, float eta, float *eta_p, Vec3 &w_t) {
     float cosTheta_i = jtx::dot(w_i, n);
-    // Ray is exiting the surface
+
+    // Check if ray is exiting the surface
+    // If so, flip parameters
     if (cosTheta_i < 0) {
-        eta = 1 / eta;
+        eta        = 1 / eta;
         cosTheta_i = -cosTheta_i;
-        n = -n;
+        n          = -n;
     }
 
+    // Update eta for user if requested
     if (eta_p) *eta_p = eta;
 
-    const float sin2Theta_i = jtx::max(0.0f, 1 - jtx::sqr(cosTheta_i)) / (eta * eta);
-    if (sin2Theta_i >= 1) return false;
+    // Compute Snell's Law
+    const float radicand = jtx::max(0.0f, 1 - jtx::sqr(cosTheta_i)) / (eta * eta);
+    if (radicand >= 1) return false;
+    const float cosTheta_t = jtx::safeSqrt(1 - radicand);
 
-    const float cosTheta_t = jtx::safeSqrt(1 - sin2Theta_i);
     w_t = -w_i / eta + (cosTheta_i / eta - cosTheta_t) * n;
+}
+
+/**
+ * Computes the fresnel reflectance of dielectric surfaces
+ * @param cosTheta_i cos of incident angle
+ * @param eta relative index of refraction: eta_t / eta_i
+ * @return fresnel reflectance
+ */
+float fresnelDielectric(float cosTheta_i, float eta) {
+    // Clamp in case of floating point issues
+    cosTheta_i = jtx::clamp(cosTheta_i, -1, 1);
+
+    // Similar to Snell's Law, we need to flip eta and cosTheta_i if our ray is exiting
+    if (cosTheta_i < 0) {
+        eta        = 1 / eta;
+        cosTheta_i = -cosTheta_i;
+    }
+
+    // Compute Snell's Law
+    const float radicand = (1 - cosTheta_i * cosTheta_i) / (eta * eta);
+    if (radicand >= 1) return 1.0f;
+    const float cosTheta_t = jtx::safeSqrt(1 - radicand);
+
+    // Compute amplitudes
+    float r_parallel      = (eta * cosTheta_i - cosTheta_t) / (eta * cosTheta_i + cosTheta_t);
+    float r_perpendicular = (cosTheta_i - eta * cosTheta_t) / (cosTheta_i + eta * cosTheta_t);
+
+    // Compute fresnel reflectance
+    return (r_parallel * r_parallel + r_perpendicular * r_perpendicular) / 2;
 }
 
 class DiffuseBRDF;
@@ -65,7 +107,7 @@ public:
             const Vec3 w_o = sampleUniformHemisphere(u1[i]);
             if (w_o.z == 0) continue;
             const float pdf_o = uniformHemispherePDF();
-            auto bs = sample(w_o, uc[i], u2[i]);
+            auto bs           = sample(w_o, uc[i], u2[i]);
             r += bs.sample * jtx::absCosTheta(bs.w_i) * jtx::absCosTheta(w_o) / (pdf_o * bs.pdf);
         }
         return r / (PI * uc.size());
@@ -74,16 +116,17 @@ public:
 
 class DiffuseBRDF {
 public:
-    explicit DiffuseBRDF(const Vec3 &R) : R_(R) {}
+    explicit DiffuseBRDF(const Vec3 &R)
+        : R_(R) {}
 
     [[nodiscard]]
-    Vec3 evaluate(const Vec3& w_o, const Vec3& w_i) const {
+    Vec3 evaluate(const Vec3 &w_o, const Vec3 &w_i) const {
         if (!jtx::sameHemisphere(w_o, w_i)) return {};
         return R_ * INV_PI;
     }
 
     [[nodiscard]]
-    BSDFSample sample(const Vec3& w_o, float uc, const Vec2f& u) const {
+    BSDFSample sample(const Vec3 &w_o, float uc, const Vec2f &u) const {
         Vec3 w_i = sampleCosineHemisphere(u);
         if (w_o.z < 0) w_i.z *= -1;
         const float pdf = cosineHemispherePDF(jtx::absCosTheta(w_i));
@@ -91,10 +134,11 @@ public:
     }
 
     [[nodiscard]]
-    float pdf(const Vec3& w_o, const Vec3& w_i) const {
+    float pdf(const Vec3 &w_o, const Vec3 &w_i) const {
         if (!jtx::sameHemisphere(w_o, w_i)) return 0;
         return cosineHemispherePDF(jtx::absCosTheta(w_i));
     }
+
 private:
     Vec3 R_;
 };
@@ -104,8 +148,10 @@ public:
     BSDF() = default;
     BSDF(const Vec3 &n_s, const Vec3 &dp_du, const BxDF &bxdf)
         : bxdf(bxdf),
-          frame(jtx::Frame::fromXZ(jtx::normalize(dp_du), n_s)){};
-    BSDF(const jtx::Frame &frame, const BxDF &bxdf) : bxdf(bxdf), frame(frame) {}
+          frame(jtx::Frame::fromXZ(jtx::normalize(dp_du), n_s)) {};
+    BSDF(const jtx::Frame &frame, const BxDF &bxdf)
+        : bxdf(bxdf),
+          frame(frame) {}
 
     Vec3 renderToLocal(const Vec3 &v) const { return frame.toLocal(v); }
     Vec3 localToRender(const Vec3 &v) const { return frame.toWorld(v); }

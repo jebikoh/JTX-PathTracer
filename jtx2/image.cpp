df66e73805a56089be82c5ee0825fba98ec4ef30
Jayden E <aedara@uw.edu>
2025-04-10 02:34:52 -0700

    Finish Image8u class

--- jtx2/image.cpp
+++ jtx2/image.cpp
@@ -1,20 +1,77 @@
 #include "image.hpp"
 
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#define TINYEXR_USE_MINIZ 0
+#define TINYEXR_USE_STB_ZLIB 1
+#define TINYEXR_IMPLEMENTATION
+#include "tinyexr.h"
+#include "ui/jvk/init.hpp"
+
 #include <filesystem>
+
+jtx::Image8u::Image8u(const uint8_t *buffer, int w, int h, int c)
+    : width(w),
+      height(h),
+      channels(c) {
+    m_data.resize(w * h * c);
+    memcpy(m_data.data(), buffer, w * h * c);
+}
+
+void jtx::Image8u::destroy() {
+    width    = 0;
+    height   = 0;
+    channels = 0;
+    m_data.resize(0);
+}
 
 jtx::Image8u jtx::Image8u::loadImage(const std::filesystem::path &path) {
     LOG_INFO("Loading 8-bit texture: {}", path.string());
 
     auto fileExt = path.extension().string();
     std::ranges::transform(fileExt, fileExt.begin(), [](const unsigned char c) { return std::tolower(c); });
-    if (!JTX_IMAGE_SUPPORTED_FORMATS.contains(fileExt)) {
+    if (!JTX_IMAGE_SUPPORTED_FORMATS_8BIT.contains(fileExt)) {
         LOG_ERROR("Unsupported image format: {}", fileExt);
         return {};
     }
 
-    if (fileExt == "exr") {
-
+    Image8u image;
+    const uint8_t *data = stbi_load(path.c_str(), &image.width, &image.height, &image.channels, 0);
+    if (!data) {
+        LOG_ERROR("Failed to load image or image was empty: {}", path.string());
+        return {};
     }
 
+    image.m_data.resize(image.width * image.height * image.channels);
+    memcpy(image.m_data.data(), data, image.width * image.height * image.channels);
+    stbi_image_free((void *) data);
 
+    LOG_INFO("Loaded 8-bit texture: {}", path.string());
+    return image;
+}
+
+jtx::Image8u jtx::Image8u::loadImage(const uint8_t *buffer, const size_t size) {
+    LOG_INFO("Loading 8-bit texture from memory");
+
+    if (!buffer || size == 0) {
+        LOG_ERROR("Invalid buffer provided");
+        return {};
+    }
+
+    Image8u image;
+    const uint8_t *data = stbi_load_from_memory(buffer, size, &image.width, &image.height, &image.channels, 0);
+    if (!data) {
+        LOG_ERROR("Failed to load image from memory");
+        return {};
+    }
+
+    image.m_data.resize(image.width * image.height * image.channels);
+    memcpy(image.m_data.data(), data, image.width * image.height * image.channels);
+    stbi_image_free((void *) data);
+
+    LOG_INFO("Loaded 8-bit texture from memory");
+    return image;
 }
--- jtx2/image.hpp
+++ jtx2/image.hpp
@@ -6,7 +6,8 @@
 
 namespace jtx {
 
-static const std::set<std::string> JTX_IMAGE_SUPPORTED_FORMATS = {"jpeg", "png", "bpm", "hdr", "psd", "tga", "gif", "pic", "psd", "pgm", "ppm", "exr"};
+static const std::set<std::string> JTX_IMAGE_SUPPORTED_FORMATS_8BIT = {"jpeg", "png", "bmp", "hdr", "psd", "tga", "gif", "pic", "psd", "pgm", "ppm"};
+static const std::set<std::string> JTX_IMAGE_SUPPORTED_FORMATS_32BIT = {"jpeg", "png", "bmp", "hdr", "psd", "tga", "gif", "pic", "psd", "pgm", "ppm", "exr"};
 
 // Image formats
 struct RGBA8u {
@@ -31,6 +32,7 @@ struct RGB32f {
  * Represents an image with 8-bit unsigned integer channels: RGBA8u and RGB8u
  */
 class Image8u {
+public:
     int width, height, channels;
 
     /**
@@ -39,15 +41,27 @@ class Image8u {
     Image8u() : width(0), height(0), channels(0) {}
 
     /**
-     * Creates an zeroed (black) image of the given size and channel count
+     * Creates a zeroed (black) image of the given size and channel count
      * @param w width
      * @param h height
      * @param c channels
      */
-    Image8u(int w, int h, int c) : width(w), height(h), channels(c), m_data(w * h * c, 0) {}
+    Image8u(const int w, const int h, const int c) : width(w), height(h), channels(c), m_data(w * h * c, 0) {}
 
     /**
-     * Loads an image from the given file. Accepts any format supported by stb and .EXR files
+     * Creates an image by copying data from a provided buffer
+     * @param buffer 8-bit uint buffer
+     * @param w buffer width
+     * @param h buffer height
+     * @param c buffer channels
+     */
+    Image8u(const uint8_t *buffer, int w, int h, int c);
+
+    void destroy();
+
+    /**
+     * Loads an image from the given file. Accepts any format supported by stb. Note
+     * float images will be converted to an 8-bit range (HDR->LDR)
      *
      * A failed load will return an empty image
      * @param path path to image file
@@ -55,13 +69,52 @@ class Image8u {
      */
     static Image8u loadImage(const std::filesystem::path &path);
 
+    /**
+     * Loads an image from a provided buffer
+     * @param buffer 8-bit data buffer
+     * @param size buffer size
+     * @return 8-bit uint image
+     */
+    static Image8u loadImage(const uint8_t *buffer, size_t size);
+
+    /**
+     * Retrieves pixel value at given coordinates.
+     * @tparam T pixel format. Must be an 8-bit format (RGB8u or RGBA8u)
+     * @param row row
+     * @param col column
+     * @return pixel value at given coordinates in given format
+     */
+    template<typename T>
+    T getPixel(int row, int col);
+
+    const uint8_t *data() const { return m_data.data(); }
+    uint8_t *data() { return m_data.data(); }
+
+    const uint8_t &operator[](const int index) const { return m_data[index]; }
+    uint8_t &operator[](const int index) { return m_data[index]; }
+
     bool isEmpty() const { return m_data.empty(); }
 private:
     std::vector<uint8_t> m_data;
-
-    static Image8u loadStbi(const std::filesystem::path &path);
-    static Image8u loadExr(const std::filesystem::path &path);
 };
+
+template<typename T>
+T Image8u::getPixel(int row, int col) {
+    LOG_ERROR("Attempted to retrieve pixel in unsupported format");
+    return T::unimplemented;
+}
+
+template<>
+inline RGB8u Image8u::getPixel<RGB8u>(const int row, const int col) {
+    const int i = row * width + col;
+    return {m_data[i], m_data[i + 1], m_data[i + 2]};
+}
+
+template<>
+inline RGBA8u Image8u::getPixel<RGBA8u>(const int row, const int col) {
+    const int i = row * width + col;
+    return {m_data[i], m_data[i + 1], m_data[i + 2]};
+}
 
 /**
  * Represents an image with 32-bit float channels: RGBA32f and RGB32f
@@ -69,6 +122,9 @@ private:
 class Image32f {
     int width, height, channels;
 
+    Image32f() : width(0), height(0), channels(0) {}
+    Image32f(const int w, const int h, const int c) : width(w), height(h), channels(c), m_data(w * h * c, 0) {}
+    Image32f(const uint8_t *buffer, int w, int h, int c);
 
 private:
     std::vector<float> m_data;

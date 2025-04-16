16868558186d5c906abda1aa97186eb2b7218f8a
Jayden E <aedara@uw.edu>
2025-04-16 00:36:31 -0700

    Add GLSL input structures and update vertex shader

---
+++ shaders/input_structures.glsl
@@ -0,0 +1,32 @@
+#extension GL_EXT_buffer_reference : require
+
+layout(buffer_reference, std430) readonly buffer Vec3Buffer {
+    vec3 data[];
+};
+
+// TODO: change this once we update UV coords to be floats
+layout(buffer_reference, std430) readonly buffer UVec2Buffer {
+    uvec2 data[];
+};
+
+layout (set = 0, binding = 0) uniform SceneData {
+    mat4 view;
+    mat4 proj;
+    mat4 viewProj;
+    vec4 cameraPos;
+    Vec3Buffer positionBuffer;
+    Vec3Buffer normalBuffer;
+    UVec2Buffer uvBuffer;
+    Vec3Buffer colorBuffer;
+} sceneData;
+
+layout (set = 1, binding = 0) uniform MaterialData {
+    vec4 ambient;
+    vec4 diffuse;
+    vec4 specular;
+    float shininess;
+} materialData;
+
+layout (set = 1, binding = 1) uniform sampler2D ambientTex;
+layout (set = 1, binding = 2) uniform sampler2D diffuseTex;
+layout (set = 1, binding = 3) uniform sampler2D specularTex;
--- shaders/mesh.vert
+++ shaders/mesh.vert
@@ -1,7 +1,18 @@
 #version 450
 
+#extension GL_GOOGLE_include_directive : require
+
+#include "input_structures.glsl"
+
+layout(push_constant) uniform PushConstants {
+    mat4 world;
+    mat4 normal;
+} pushConstants;
+
 layout(location = 0) out vec3 outColor;
 
 void main() {
-    outColor = vec3(1.0, 0.0, 0.0); // Set the output color to red
+    vec3 v = sceneData.positionBuffer.data[gl_VertexIndex];
+    gl_Position = sceneData.viewProj * pushConstants.world * vec4(v, 1.0f);
+    outColor = vec3(1.0, 0.0, 0.0); // output color always to red for debugging
 }

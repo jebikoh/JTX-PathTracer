93d163f5e02fe0ca354b978f9c2feaa67e408663
Jayden E. <jaydenedara@gmail.com>
2025-07-07 03:25:03 -0700

    Remove JTX1 source, update README, fix assets

--- src/util/aabb.cpp
+++
@@ -1,4 +0,0 @@
-#include "aabb.hpp"
-
-const AABB AABB::EMPTY    = AABB(Vec3(INF, INF, INF), Vec3(-INF, -INF, -INF));
-const AABB AABB::UNIVERSE = AABB(Vec3(-INF, -INF, -INF), Vec3(INF, INF, INF));
--- src/util/interval.cpp
+++
@@ -1,4 +0,0 @@
-#include "interval.hpp"
-
-const Interval Interval::EMPTY = Interval(INF, -INF);
-const Interval Interval::UNIVERSE = Interval(-INF, INF);
